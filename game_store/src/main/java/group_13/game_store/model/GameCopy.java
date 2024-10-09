8d05c572cb0e2e2f7b8c50732d86795f66e3818f
Garrett Woodson <garrettwoodson@icloud.com>
2024-10-09 16:13:48 -0400

    Annotations for GameCopy.java and Promotions.java

--- game_store/src/main/java/group_13/game_store/model/GameCopy.java
+++ game_store/src/main/java/group_13/game_store/model/GameCopy.java
@@ -1,5 +1,12 @@
 package group_13.game_store.model;
 
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+
 /*PLEASE DO NOT EDIT THIS CODE*/
 /*This code was generated using the UMPLE 1.34.0.7242.6b8819789 modeling language!*/
 
@@ -7,6 +14,7 @@ package group_13.game_store.model;
 
 // line 86 "model.ump"
 // line 200 "model.ump"
+@Entity
 public class GameCopy
 {
 
@@ -15,68 +23,67 @@ public class GameCopy
   //------------------------
 
   //GameCopy Associations
-  private Order order;
-  private Game game;
+  private Key key;
 
   //------------------------
   // CONSTRUCTOR
   //------------------------
 
-  public GameCopy(Order aOrder, Game aGame)
+  // Hibernate needs a no-args constructor, but it can be protected
+  protected GameCopy()
   {
-    if (!setOrder(aOrder))
-    {
-      throw new RuntimeException("Unable to create GameCopy due to aOrder. See https://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
-    }
-    if (!setGame(aGame))
-    {
-      throw new RuntimeException("Unable to create GameCopy due to aGame. See https://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
-    }
   }
 
-  //------------------------
-  // INTERFACE
-  //------------------------
-  /* Code from template association_GetOne */
-  public Order getOrder()
+  public GameCopy(Key key)
   {
-    return order;
-  }
-  /* Code from template association_GetOne */
-  public Game getGame()
-  {
-    return game;
-  }
-  /* Code from template association_SetUnidirectionalOne */
-  public boolean setOrder(Order aNewOrder)
-  {
-    boolean wasSet = false;
-    if (aNewOrder != null)
-    {
-      order = aNewOrder;
-      wasSet = true;
-    }
-    return wasSet;
-  }
-  /* Code from template association_SetUnidirectionalOne */
-  public boolean setGame(Game aNewGame)
-  {
-    boolean wasSet = false;
-    if (aNewGame != null)
-    {
-      game = aNewGame;
-      wasSet = true;
-    }
-    return wasSet;
+    this.key = key;
   }
 
-  public void delete()
+  public Key getKey()
   {
-    order = null;
-    game = null;
+    return key;
   }
 
-}
+  @Embeddable
+    public static class Key implements Serializable {
+      @ManyToOne
+      private Order order;
+      @ManyToOne
+      private Game game;
+
+      public Key() {
+        super();
+      }
+
+      public Key(Order order, Game game) {
+        this.order = order;
+        this.game = game;
+      }
+
+      public Order getOrder() {
+        return order;
+      }
+
+      public Game getGame() {
+        return game;
+      }
+
+      @Override
+      public boolean equals(Object obj) {
+        if (!(obj instanceof Key)) {
+          return false;
+        }
+        Key that = (Key) obj;
+        return this.getOrder().getOrderID() == that.getOrder().getOrderID()
+            && this.getGame().getGameID() == that.getGame().getGameID();
+      }
+
+      @Override
+      public int hashCode() {
+        return Objects.hash(this.getOrder().getOrderID(), this.getGame().getGameID());
+      }
+    }
+  }
 
 
 
