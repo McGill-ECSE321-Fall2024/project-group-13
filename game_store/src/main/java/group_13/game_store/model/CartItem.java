422a08158c80b2aea504716b51fba8b6acddf9fd
NicDole <116467268+NicDole@users.noreply.github.com>
2024-10-10 11:30:47 -0400

    Updated CartItem.java to follow structure in Tutorial
    Adjusted the CartItem class based to reflect the structure of composite IDs shown in tutorial

--- game_store/src/main/java/group_13/game_store/model/CartItem.java
+++ game_store/src/main/java/group_13/game_store/model/CartItem.java
@@ -5,10 +5,12 @@ package group_13.game_store.model;
 
 import java.io.Serializable;
 import java.util.Objects;
+
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.EmbeddedId;
+
 import jakarta.persistence.MapsId;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
@@ -17,7 +19,6 @@ import jakarta.persistence.Column;
 // line 52 "model.ump"
 // line 183 "model.ump"
 @Entity
-@Table(name = "cart_assistant")
 public class CartItem implements Serializable
 {
 
@@ -25,39 +26,28 @@ public class CartItem implements Serializable
   // MEMBER VARIABLES
   //------------------------
   @EmbeddedId
-  private CartItemId id;
+  private Key key;
   
   //CartItem Attributes
-  @Column(name = "no_game_copies", nullable = false)
+  @GeneratedValue
   private int quantity;
 
-  //CartItem Associations
-  @MapsId("username")
-  @ManyToOne
-  @JoinColumn(name = "username", nullable = false)
-  private Customer customer;
-
-  @MapsId("gameId")
-  @ManyToOne
-  @JoinColumn(name = "game_id", nullable = false)
-  private Game addedGames;
 
   //------------------------
   // CONSTRUCTOR
   //------------------------
-
-  public CartItem(int aQuantity, Customer aCustomer, Game aAddedGames)
-  {
-    quantity = aQuantity;
-    if (!setCustomer(aCustomer))
-    {
-      throw new RuntimeException("Unable to create CartItem due to aCustomer. See https://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
-    }
-    if (!setAddedGames(aAddedGames))
-    {
-      throw new RuntimeException("Unable to create CartItem due to aAddedGames. See https://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
-    }
+  protected CartItem(){
   }
+
+  public CartItem(Key key){
+    this.key = key;
+  }
+
+  public Key getKey()
+  {
+    return key;
+  }
+
 
   //------------------------
   // INTERFACE
@@ -75,102 +65,48 @@ public class CartItem implements Serializable
   {
     return quantity;
   }
-  /* Code from template association_GetOne */
-  public Customer getCustomer()
-  {
-    return customer;
-  }
-  /* Code from template association_GetOne */
-  public Game getAddedGames()
-  {
-    return addedGames;
-  }
-  /* Code from template association_SetUnidirectionalOne */
-  public boolean setCustomer(Customer aNewCustomer)
-  {
-    boolean wasSet = false;
-    if (aNewCustomer != null)
-    {
-      customer = aNewCustomer;
-      wasSet = true;
-    }
-    return wasSet;
-  }
-  /* Code from template association_SetUnidirectionalOne */
-  public boolean setAddedGames(Game aNewAddedGames)
-  {
-    boolean wasSet = false;
-    if (aNewAddedGames != null)
-    {
-      addedGames = aNewAddedGames;
-      wasSet = true;
-    }
-    return wasSet;
-  }
-
-  public void delete()
-  {
-    customer = null;
-    addedGames = null;
-  }
-
-
-  public String toString()
-  {
-    return super.toString() + "["+
-            "quantity" + ":" + getQuantity()+ "]" + System.getProperties().getProperty("line.separator") +
-            "  " + "customer = "+(getCustomer()!=null?Integer.toHexString(System.identityHashCode(getCustomer())):"null") + System.getProperties().getProperty("line.separator") +
-            "  " + "addedGames = "+(getAddedGames()!=null?Integer.toHexString(System.identityHashCode(getAddedGames())):"null");
-  }
-}
-
+  
 
 @Embeddable
 class CartItemId implements Serializable {
-  @Column(name = "username")
-  private String username;
+  @ManyToOne
+  private UserAccount userAccount;
 
-  @Column(name = "game_id")
-  private Long gameId;
+  @ManyToOne
+  private Game game;
 
-  // Default constructor
-  public CartItemId() {}
-
-  // Constructor
-  public CartItemId(String username, Long gameId) {
-    this.username = username;
-    this.gameId = gameId;
+  public Key() {
+    super();
   }
 
-  // Getters and setters
-  public String getUsername() {
-    return username;
+  public key(UserAccount userAccount, Game game) {
+    this.userAccount = userAccount,
+    this.game = game;
   }
 
-  public void setUsername(String username) {
-    this.username = username;
+  public UserAccount getUserAccount(){
+    return userAccount;
   }
 
-  public Long getGameId() {
-    return gameId;
+  public Game getGame(){
+    return game;
   }
-
-  public void setGameId(Long gameId) {
-    this.gameId = gameId;
-  }
+  
 
   // Equals and hashCode
   @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    CartItemId that = (CartItemId) o;
-    return username.equals(that.username) && gameId.equals(that.gameId);
+  public boolean equals(Object obj) {
+    if (!(obj instanceof Key)) {
+       return false;
+    }
+    Key that = (Key) obj;
+    return this.getUserAccount().getUserAccountID() == that.getUserAccount().getUserAccountID()
+            && this.getGame().getGameID() == that.getGame().getGameID();
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(username, gameId);
+    return Objects.hash(this.getUserAccount().getUserAccountID(), this.getGame().getGameID());
   }
 }
 
