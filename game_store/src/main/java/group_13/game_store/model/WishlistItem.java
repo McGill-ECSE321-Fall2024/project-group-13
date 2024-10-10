1faa61375a21f5be2de796257d4dadf95bf65707
NicDole <116467268+NicDole@users.noreply.github.com>
2024-10-10 09:23:08 -0400

    Annotations and added @embeddable class for wishlistItem.java
    Similar to cart item, I added the necessary annotations and the embeddable class to ensure the composite key functionality

--- game_store/src/main/java/group_13/game_store/model/WishlistItem.java
+++ game_store/src/main/java/group_13/game_store/model/WishlistItem.java
@@ -7,15 +7,28 @@ package group_13.game_store.model;
 
 // line 48 "model.ump"
 // line 178 "model.ump"
-public class WishlistItem
+@Entity
+@Table(name = "wishlist_assistant")
+public class WishlistItem implements Serializable
 {
 
   //------------------------
   // MEMBER VARIABLES
   //------------------------
 
+  //Composite Key
+  @EmbeddedId
+  private WishlistItemId id;
+  
   //WishlistItem Associations
+  @MapsId("username")
+  @ManyToOne
+  @JoinColumn(name = "username", nullable = false)
   private Customer customer;
+
+  @MapsId("gameId")
+  @ManyToOne
+  @JoinColumn(name = "game_id", nullable = false)
   private Game addedgames;
 
   //------------------------
@@ -76,4 +89,54 @@ public class WishlistItem
     addedgames = null;
   }
 
+}
+
+
+@Embeddable
+class WishlistItemId implements Serializable {
+  @Column(name = "username")
+  private String username;
+
+  @Column(name = "game_id")
+  private Long gameId;
+
+  // Default constructor
+  public WishlistItemId() {}
+
+  // Constructor
+  public WishlistItemId(String username, Long gameId) {
+    this.username = username;
+    this.gameId = gameId;
+  }
+
+  // Getters and setters
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public Long getGameId() {
+    return gameId;
+  }
+
+  public void setGameId(Long gameId) {
+    this.gameId = gameId;
+  }
+
+  // Equals and hashCode
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    WishlistItemId that = (WishlistItemId) o;
+    return username.equals(that.username) && gameId.equals(that.gameId);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(username, gameId);
+  }
 }
