52e4419497cb99e61d26a72ab256242b94fdef93
PromitHossain <promit.hossain@mail.mcgill.ca>
2024-10-11 14:20:49 -0400

    Finished making the tests for Game.

--- game_store/src/test/java/group_13/game_store/GameRepositoryTests.java
+++ game_store/src/test/java/group_13/game_store/GameRepositoryTests.java
@@ -13,12 +13,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 // will need GameCategory and Promotion for tests 
 import group_13.game_store.model.Game;
 import group_13.game_store.model.GameCategory;
-import group_13.game_store.model.GameCategory.VisibilityStatus;
-// need to modify this issue
-//import group_13.game_store.model.Game.VisibilityStatus;
 import group_13.game_store.model.Promotion;
-// hopefully this line does not cause an issue...
 import group_13.game_store.repository.GameRepository;
+import group_13.game_store.repository.GameCategoryRepository;
+import group_13.game_store.repository.PromotionRepository;
 
 @SpringBootTest
 public class GameRepositoryTests {
@@ -26,26 +24,61 @@ public class GameRepositoryTests {
     @Autowired
     private GameRepository gameRepo;
     @Autowired
-    private Promotion promo;
+    private PromotionRepository promoRepo;
     @Autowired
-    private GameCategory gameCategory;
+    private GameCategoryRepository gameCategoryRepo;
 
     @BeforeEach
 	@AfterEach
 	public void clearDatabase() {
 		gameRepo.deleteAll();
+        promoRepo.deleteAll();
+        gameCategoryRepo.deleteAll();
 	}
 
     // first test: check if the Game actually exists in the database
     @Test
 	public void testCreateAndReadGame() {
         // arrange
-
-        GameCategory randomCategory = new GameCategory("this genre involves X and y", VisibilityStatus.Visible, "generic genre"); 
-        Promotion limitedPromo = new Promotion(15, Date.valueOf("2024-10-21"), Date.valueOf("2024-10-31"), "Halloween Deal", "Discounts, because Halloween is soon");
-        //Game randomGame = new Game("Generic title", "Long description", "Image of game", 50, 14.99, "18+", VisibilityStatus.Visible, randomCategory);
+        // need to create fields for game to be instantiated
+        GameCategory randomCategory = new GameCategory("this genre involves X and y", GameCategory.VisibilityStatus.Visible, "generic genre"); 
+        Promotion randomPromo = new Promotion(15, Date.valueOf("2024-10-21"), Date.valueOf("2024-10-31"), "Halloween Deal", "Discounts, because Halloween is soon");
+        
+        Game randomGame = new Game("Generic title", "Long description", "Image of game", 50, 14.99, "18+", Game.VisibilityStatus.Visible, randomCategory);
+        randomGame.setPromotion(randomPromo);
+        
+        randomCategory = gameCategoryRepo.save(randomCategory);
+        randomPromo = promoRepo.save(randomPromo);
+        randomGame = gameRepo.save(randomGame);
+       
+        int randomGameId = randomGame.getGameID();
+        // act
+        Game randomGameFromDB = gameRepo.findByGameID(randomGameId);
+    
 
         //assert
+        assertNotNull(randomGameFromDB);
+        // checking all fields of Game
+        assertEquals(randomGame.getTitle(), randomGameFromDB.getTitle());
+        assertEquals(randomGame.getDescription(), randomGameFromDB.getDescription());
+        assertEquals(randomGame.getImg(), randomGameFromDB.getImg());
+        assertEquals(randomGame.getStock(), randomGameFromDB.getStock());
+        assertEquals(randomGame.getPrice(), randomGameFromDB.getPrice());
+        assertEquals(randomGame.getParentalRating(), randomGameFromDB.getParentalRating());
+        assertEquals(randomGame.getStatus(), randomGameFromDB.getStatus());
+
+        assertEquals(randomGame.getCategory().getCategoryID(), randomGameFromDB.getCategory().getCategoryID());
+        assertEquals(randomGame.getCategory().getDescription(), randomGameFromDB.getCategory().getDescription());
+        assertEquals(randomGame.getCategory().getName(), randomGameFromDB.getCategory().getName());
+        assertEquals(randomGame.getCategory().getStatus(), randomGameFromDB.getCategory().getStatus());
+
+        assertEquals(randomGame.getPromotion().getDescription(), randomGameFromDB.getPromotion().getDescription());
+        assertEquals(randomGame.getPromotion().getEndDate(), randomGameFromDB.getPromotion().getEndDate());
+        assertEquals(randomGame.getPromotion().getPercentage(), randomGameFromDB.getPromotion().getPercentage());
+        assertEquals(randomGame.getPromotion().getStartDate(), randomGameFromDB.getPromotion().getStartDate());
+        assertEquals(randomGame.getPromotion().getPromotionID(), randomGameFromDB.getPromotion().getPromotionID());
+        assertEquals(randomGame.getPromotion().getTitle(), randomGameFromDB.getPromotion().getTitle());
+
     }
 
 
