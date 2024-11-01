3f175d40ec841c2b2579bff7a33f56c0097904db
timamora <timroma6@gmail.com>
2024-10-31 21:09:13 -0400

    Introduced a way to find what games are associated to a customer in game repo

--- game_store/src/main/java/group_13/game_store/repository/GameRepository.java
+++ game_store/src/main/java/group_13/game_store/repository/GameRepository.java
@@ -1,9 +1,21 @@
 package group_13.game_store.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import group_13.game_store.model.Customer;
 import group_13.game_store.model.Game;
 
 public interface GameRepository extends CrudRepository<Game, Integer> {
     // allows instantiation of an Game instance that is stored in the local database by its unique ID
     public Game findByGameID(int gameID);
+
+    @Query("SELECT DISTINCT g FROM Game g " +
+           "JOIN g.gameCopies gc " +
+           "JOIN gc.order o " +
+           "WHERE o.customer = :customer")
+    List<Game> findGamesByCustomer(@Param("customer") Customer customer);
 }
