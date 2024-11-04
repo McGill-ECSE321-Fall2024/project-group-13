8ef56bdbd7c25622c1352fd27f94bc25c8641f9e
NicDole <nicodolgopolyy@gmail.com>
2024-11-04 15:16:42 -0500

    added the GameStoreManagementService class and one function in GameRepository

--- game_store/src/main/java/group_13/game_store/repository/GameRepository.java
+++ game_store/src/main/java/group_13/game_store/repository/GameRepository.java
@@ -3,7 +3,11 @@ package group_13.game_store.repository;
 import org.springframework.data.repository.CrudRepository;
 import group_13.game_store.model.Game;
 
+import java.util.List;
+
 public interface GameRepository extends CrudRepository<Game, Integer> {
     // allows instantiation of an Game instance that is stored in the local database by its unique ID
     public Game findByGameID(int gameID);
+
+    List<Game> findByStatusIn(Game.VisibilityStatus status);
 }
