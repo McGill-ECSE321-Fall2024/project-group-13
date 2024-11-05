2cd6a561f31e311acaa6391e9d96927dcf45f864
MarrecBois <marrec.bois@gmail.com>
2024-11-05 10:24:34 -0500

    Bug fixes with data initializer

--- game_store/src/main/java/group_13/game_store/initializer/DataInitializer.java
+++ game_store/src/main/java/group_13/game_store/initializer/DataInitializer.java
@@ -3,24 +3,24 @@ package group_13.game_store.initializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import group_13.game_store.model.UserAccount;
-import group_13.game_store.repository.UserAccountRepository;
+import group_13.game_store.model.Owner;
+import group_13.game_store.repository.OwnerRepository;
 import jakarta.annotation.PostConstruct;
 
 @Component
 public class DataInitializer {
     
     @Autowired
-    private UserAccountRepository userRepo;
+    private OwnerRepository ownerRepo;
 
     @PostConstruct
     public void initializeData() {
         // Check if the default owner account already exists, and if not, create it
-        if (userRepo.findByUsername("owner") == null) {
-            UserAccount owner = new UserAccount("Owner", "owner", "owner@gmail.com", "ownerPassword", "123-456-7890");
+        if (ownerRepo.findByUsername("owner") == null) {
+            Owner owner = new Owner("Owner", "owner", "owner@gmail.com", "ownerPassword", "123-456-7890");
             
             // Save to database so that there is a default owner
-            userRepo.save(owner);
+            ownerRepo.save(owner);
         }
     }
 }
