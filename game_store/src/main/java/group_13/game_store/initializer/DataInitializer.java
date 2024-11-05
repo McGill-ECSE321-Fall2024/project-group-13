9d775fc75007c14a12c6edd21c94a7b5d3e24c94
MarrecBois <marrec.bois@gmail.com>
2024-11-05 11:01:44 -0500

    Hashes the default owner password

--- game_store/src/main/java/group_13/game_store/initializer/DataInitializer.java
+++ game_store/src/main/java/group_13/game_store/initializer/DataInitializer.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 
 import group_13.game_store.model.Owner;
 import group_13.game_store.repository.OwnerRepository;
+import group_13.game_store.service.AccountService;
 import jakarta.annotation.PostConstruct;
 
 @Component
@@ -17,7 +18,8 @@ public class DataInitializer {
     public void initializeData() {
         // Check if the default owner account already exists, and if not, create it
         if (ownerRepo.findByUsername("owner") == null) {
-            Owner owner = new Owner("Owner", "owner", "owner@gmail.com", "ownerPassword", "123-456-7890");
+            String hashedPassword = AccountService.hashPassword("ownerPassword");
+            Owner owner = new Owner("Owner", "owner", "owner@gmail.com", hashedPassword, "123-456-7890");
             
             // Save to database so that there is a default owner
             ownerRepo.save(owner);
