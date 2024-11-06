613dac6e6d990c90fa4b8836e07362ef1030b4ed
WLBlackmore <william.blackmore@mail.mcgill.ca>
2024-11-06 18:08:44 -0500

    add GameController prototype

---
+++ game_store/src/main/java/group_13/game_store/controller/GameController.java
@@ -0,0 +1,22 @@
+package group_13.game_store.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import group_13.game_store.service.BrowsingService;
+import group_13.game_store.service.GameStoreManagementService;
+import group_13.game_store.service.ReviewService;
+
+@RestController
+public class GameController {
+    @Autowired
+    BrowsingService browsingService;
+
+    @Autowired
+    GameStoreManagementService gameStoreManagementService;
+
+    @Autowired
+    ReviewService reviewService;
+
+    
+}
