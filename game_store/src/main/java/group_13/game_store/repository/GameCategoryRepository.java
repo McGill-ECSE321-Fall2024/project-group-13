9d7987522fd3eca92f36e992da264e4254fb0cc1
Garrett Woodson <garrettwoodson@icloud.com>
2024-11-07 18:47:15 -0500

    updated game category repo and game management service to allow for all controller methods

--- game_store/src/main/java/group_13/game_store/repository/GameCategoryRepository.java
+++ game_store/src/main/java/group_13/game_store/repository/GameCategoryRepository.java
@@ -1,9 +1,14 @@
 package group_13.game_store.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+
 import group_13.game_store.model.GameCategory;
 
 public interface GameCategoryRepository extends CrudRepository<GameCategory, Integer> {
     // allows instantiation of an Category instance that is stored in the local database by its unique ID
     public GameCategory findByCategoryID(int categoryID);
+
+    List<GameCategory> findByStatusIn(List<GameCategory.VisibilityStatus> statuses);
 }
