c07447d5552b60de9a1c117f09ae7004ddfe3c62
Garrett Woodson <garrettwoodson@icloud.com>
2024-11-07 19:01:45 -0500

    Created employee, customer, user account response dtos

--- game_store/src/main/java/group_13/game_store/dto/EmployeeResponseDto.java
+++ game_store/src/main/java/group_13/game_store/dto/EmployeeResponseDto.java
@@ -1,39 +1,16 @@
 package group_13.game_store.dto;
 
-import group_13.game_store.model.GameCategory;
-import group_13.game_store.model.GameCategory.VisibilityStatus;
+import group_13.game_store.model.Employee;
 
-public class EmployeeResponseDto {
-    private int categoryID;
-    private String description;
-    private VisibilityStatus status;
-    private String name;
+public class EmployeeResponseDto extends UserAccountResponseDto {
+    private boolean isActive;
 
-    protected EmployeeResponseDto() {
+    public EmployeeResponseDto(Employee aEmployeeAccount) {
+        super(aEmployeeAccount);
+        this.isActive = aEmployeeAccount.getIsActive();
     }
 
-    public EmployeeResponseDto(GameCategory category)
-    {
-        this.categoryID = category.getCategoryID();
-        this.description = category.getDescription();
-        this.status = category.getStatus();
-        this.name = category.getName();
+    public boolean getIsActive() {
+        return isActive;
     }
-
-
-	public int getId() {
-		return categoryID;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public String getdescription() {
-		return description;
-	}
-
-	public VisibilityStatus getStatus() {
-		return status;
-	}
 }
