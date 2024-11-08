4a9e2b82a8f93ab153ae33b1ab72e9b26eb8c015
timamora <timroma6@gmail.com>
2024-11-08 18:00:17 -0500

    Split the like management method into one like and one remove like

--- game_store/src/main/java/group_13/game_store/controller/ReviewController.java
+++ game_store/src/main/java/group_13/game_store/controller/ReviewController.java
@@ -1,6 +1,7 @@
 package group_13.game_store.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.DeleteExchange;
 
 import group_13.game_store.dto.ReplyRequestDto;
 import group_13.game_store.dto.ReplyResponseDto;
@@ -114,25 +116,34 @@ public class ReviewController {
     }
 
     /*
-     * /games/{gameID}/reviews/{reviewID}/likes [PUT]
+     * /games/{gameID}/reviews/{reviewID}/likes [POST, DELETE]
      */
-    @PutMapping("/games/{gameID}/reviews/{reviewID}/likes")
-    public ReviewResponseDto updatesLikes(@PathVariable int reviewID,
-            @RequestParam String loggedInUsername,
-            @RequestParam boolean addLike) {
+    @PostMapping("/games/{gameID}/reviews/{reviewID}/likes")
+    public ReviewResponseDto addLike(@PathVariable int reviewID,
+            @RequestParam String loggedInUsername
+        ) {
         // Check if the user has permission to like a review
         if (!accountService.hasPermission(loggedInUsername, 2)) {
             throw new IllegalArgumentException("User does not have permission to like a review.");
         }
 
-        if(addLike) {
-            reviewService.addLike(reviewID, loggedInUsername);
-        } else {
-            reviewService.removeLike(reviewID, loggedInUsername);
-        }
+        reviewService.addLike(reviewID, loggedInUsername);
 
         return new ReviewResponseDto(reviewService.getReview(reviewID));
+    }
+
+    @DeleteMapping("/games/{gameID}/reviews/{reviewID}/likes")
+    public ReviewResponseDto removeLike(@PathVariable int reviewID,
+            @RequestParam String loggedInUsername) {
+        // Check if the user has permission to like a review
+        if (!accountService.hasPermission(loggedInUsername, 2)) {
+            throw new IllegalArgumentException("User does not have permission to like a review.");
         }
+
+        reviewService.removeLike(reviewID, loggedInUsername);  
+
+        return new ReviewResponseDto(reviewService.getReview(reviewID));
+    }
 
 
     /*
