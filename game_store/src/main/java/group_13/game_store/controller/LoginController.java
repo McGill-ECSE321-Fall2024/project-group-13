0dcb3817266b63ee4dcb82278119ee62065db1ac
MarrecBois <marrec.bois@gmail.com>
2024-11-13 18:06:09 -0500

    Added Login Controller for users to login to their account

---
+++ game_store/src/main/java/group_13/game_store/controller/LoginController.java
@@ -0,0 +1,38 @@
+package group_13.game_store.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import group_13.game_store.dto.LoginRequestDto;
+import group_13.game_store.service.AccountService;
+
+@RestController
+public class LoginController {
+    @Autowired
+    AccountService accountService;
+
+    /**
+     * Logs a user into the system after validating their credentials (Only guests)
+     * 
+     * @param loggedInUsername  The user name of the person currently logged in. If no one is logged in, it is equal to 'guest'.
+     * @param loginRequestDto   Login credentials (username and password) to login with
+     * @return                  The user name of the user that logged in, if successful
+     */
+    @PostMapping("/login")
+    public String loginToUserAccount(@RequestParam String loggedInUsername, @RequestBody LoginRequestDto loginRequestDto) {
+        if (accountService.hasPermissionAtLeast(loggedInUsername, 1)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You are already logged in");
+        }
+
+        String logUsername = accountService.loginToAccount(
+            loginRequestDto.getUsername(), 
+            loginRequestDto.getPassword());
+        
+        return logUsername;
+    }
+}
