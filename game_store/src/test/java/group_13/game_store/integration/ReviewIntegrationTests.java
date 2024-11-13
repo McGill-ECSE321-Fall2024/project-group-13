501bdd54b852dff26a25625ae05f466af90bcc30
timamora <timroma6@gmail.com>
2024-11-12 20:00:12 -0500

    started work on the integration tests for review

--- game_store/src/test/java/group_13/game_store/integration/ReviewIntegrationTests.java
+++ game_store/src/test/java/group_13/game_store/integration/ReviewIntegrationTests.java
@@ -1,5 +1,34 @@
 package group_13.game_store.integration;
 
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import group_13.game_store.repository.ReviewRepository;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(OrderAnnotation.class)
+@TestInstance(Lifecycle.PER_CLASS)
 public class ReviewIntegrationTests {
+    @Autowired
+    private TestRestTemplate client;
+    
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Test
+    @Order(1)
+    public void CreateReview() {
+        // Create a review
+        
+    }
     
 }
