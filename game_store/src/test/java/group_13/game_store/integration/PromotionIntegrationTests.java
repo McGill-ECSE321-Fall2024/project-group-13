167ce6e7caadd9f474c25ab896b0b0613f8c0e94
timamora <timroma6@gmail.com>
2024-11-13 18:04:46 -0500

    Finished createPromotionTest

--- game_store/src/test/java/group_13/game_store/integration/PromotionIntegrationTests.java
+++ game_store/src/test/java/group_13/game_store/integration/PromotionIntegrationTests.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.TestMethodOrder;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.sql.Date;
 import java.time.LocalDate;
@@ -87,6 +88,203 @@ public class PromotionIntegrationTests {
         assertEquals(percentage, savedPromotion.getPercentage());
         assertEquals(description, savedPromotion.getDescription());
         assertEquals(title, savedPromotion.getTitle());    
+    }
+
+    @Test
+    @Order(2)
+    public void testCreatePromotion_Fail_Percentage() {
+        //Check the current amount of promotions in the database
+        long previousPromotionCount = promotionRepository.count();
+
+        // Check if the promotion was not saved in the database
+        String loggedInUsername = "owner";
+
+        int percentage = 101;
+        String description = "Fall sale!";
+        Date startDate = Date.valueOf(LocalDate.of(2024, 9, 14));
+        Date endDate = Date.valueOf(LocalDate.of(2025, 1, 14)); //Make it a valid promotion
+        String title = "Fall Sale";
+
+        // Create a promotion request
+        PromotionRequestDto promotionRequest = new PromotionRequestDto(percentage, description, startDate, endDate, title);
+
+        ResponseEntity<PromotionResponseDto> response = client.postForEntity(
+            "/games/promotions?loggedInUsername=" + loggedInUsername, 
+            promotionRequest, 
+            PromotionResponseDto.class
+        );
+
+        // Assert
+        assertNotNull(response);
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+
+        // Check if the promotion was not saved in the database
+        assertEquals(previousPromotionCount, promotionRepository.count());
+    }
+
+    @Test
+    @Order(3)
+    public void testCreatePromotion_UserLacksPermission() {
+        //Check the current amount of promotions in the database
+        long previousPromotionCount = promotionRepository.count();
+
+
+        String loggedInUsername = "guest";
+
+        int percentage = 10;
+        String description = "Fall sale!";
+        Date startDate = Date.valueOf(LocalDate.of(2024, 9, 14));
+        Date endDate = Date.valueOf(LocalDate.of(2025, 1, 14)); //Make it a valid promotion
+        String title = "Fall Sale";
+
+        // Create a promotion request
+        PromotionRequestDto promotionRequest = new PromotionRequestDto(percentage, description, startDate, endDate, title);
+
+        ResponseEntity<String> response = client.postForEntity(
+            "/games/promotions?loggedInUsername=" + loggedInUsername, 
+            promotionRequest, 
+            String.class
+        );
+
+        // Assert
+        assertNotNull(response);
+        assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode());
+        assertTrue(response.getBody().contains("User does not have permission to create promotions."));
+
+        // Check if the promotion was not saved in the database
+        assertEquals(previousPromotionCount, promotionRepository.count());
+    }
+
+    @Test
+    @Order(4)
+    public void testCreatePromotion_MissingTitle() {
+        //Check the current amount of promotions in the database
+        long previousPromotionCount = promotionRepository.count();
+
+        // Check if the promotion was not saved in the database
+        String loggedInUsername = "owner";
+
+        int percentage = 10;
+        String description = "Fall sale!";
+        Date startDate = Date.valueOf(LocalDate.of(2024, 9, 14));
+        Date endDate = Date.valueOf(LocalDate.of(2025, 1, 14)); //Make it a valid promotion
+        String title = "";
+
+        // Create a promotion request
+        PromotionRequestDto promotionRequest = new PromotionRequestDto(percentage, description, startDate, endDate, title);
+
+        ResponseEntity<String> response = client.postForEntity(
+            "/games/promotions?loggedInUsername=" + loggedInUsername, 
+            promotionRequest, 
+            String.class
+        );
+
+        // Assert
+        assertNotNull(response);
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertTrue(response.getBody().contains("Title must be provided."));
+
+        // Check if the promotion was not saved in the database
+        assertEquals(previousPromotionCount, promotionRepository.count());
+    }
+
+    @Test
+    @Order(5)
+    public void testCreatePromotion_MissingDescription() {
+        //Check the current amount of promotions in the database
+        long previousPromotionCount = promotionRepository.count();
+
+        // Check if the promotion was not saved in the database
+        String loggedInUsername = "owner";
+
+        int percentage = 10;
+        String description = "";
+        Date startDate = Date.valueOf(LocalDate.of(2024, 9, 14));
+        Date endDate = Date.valueOf(LocalDate.of(2025, 1, 14)); //Make it a valid promotion
+        String title = "Fall Sale";
+
+        // Create a promotion request
+        PromotionRequestDto promotionRequest = new PromotionRequestDto(percentage, description, startDate, endDate, title);
+
+        ResponseEntity<String> response = client.postForEntity(
+            "/games/promotions?loggedInUsername=" + loggedInUsername, 
+            promotionRequest, 
+            String.class
+        );
+
+        // Assert
+        assertNotNull(response);
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertTrue(response.getBody().contains("Description must be provided."));
+
+        // Check if the promotion was not saved in the database
+        assertEquals(previousPromotionCount, promotionRepository.count());
+    }
+
+    @Test
+    @Order(6)
+    public void testCreatePromotion_MissingStartDate() {
+        //Check the current amount of promotions in the database
+        long previousPromotionCount = promotionRepository.count();
+
+        // Check if the promotion was not saved in the database
+        String loggedInUsername = "owner";
+
+        int percentage = 10;
+        String description = "Fall sale!";
+        Date startDate = null;
+        Date endDate = Date.valueOf(LocalDate.of(2025, 1, 14)); //Make it a valid promotion
+        String title = "Fall Sale";
+
+        // Create a promotion request
+        PromotionRequestDto promotionRequest = new PromotionRequestDto(percentage, description, startDate, endDate, title);
+
+        ResponseEntity<String> response = client.postForEntity(
+            "/games/promotions?loggedInUsername=" + loggedInUsername, 
+            promotionRequest, 
+            String.class
+        );
+
+        // Assert
+        assertNotNull(response);
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertTrue(response.getBody().contains("Start date and end date must be provided."));
+
+        // Check if the promotion was not saved in the database
+        assertEquals(previousPromotionCount, promotionRepository.count());
+    }
+
+    @Test
+    @Order(7)
+    public void testCreatePromotion_MissingEndDate() {
+        //Check the current amount of promotions in the database
+        long previousPromotionCount = promotionRepository.count();
+
+        // Check if the promotion was not saved in the database
+        String loggedInUsername = "owner";
+
+        int percentage = 10;
+        String description = "Fall sale!";
+        Date startDate = Date.valueOf(LocalDate.of(2024, 9, 14));
+        Date endDate = null; //Make it a valid promotion
+        String title = "Fall Sale";
+
+        // Create a promotion request
+        PromotionRequestDto promotionRequest = new PromotionRequestDto(percentage, description, startDate, endDate, title);
+
+        ResponseEntity<String> response = client.postForEntity(
+            "/games/promotions?loggedInUsername=" + loggedInUsername, 
+            promotionRequest, 
+            String.class
+        );
+
+        // Assert
+        assertNotNull(response);
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertTrue(response.getBody().contains("Start date and end date must be provided."));
+
+        // Check if the promotion was not saved in the database
+        assertEquals(previousPromotionCount, promotionRepository.count());
     }
     
 }
