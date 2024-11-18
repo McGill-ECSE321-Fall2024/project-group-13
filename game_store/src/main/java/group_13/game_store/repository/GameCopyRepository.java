4cedabe4deb5057a61eac811a9107f2bdfb63ad0
PromitHossain <promit.hossain@mail.mcgill.ca>
2024-11-17 22:00:51 -0500

    Added an extra repository query method in CopyRepository. Fixed ReturnOrder service method to be capable of returning more than one copy of a game as well as multiple different games.

--- game_store/src/main/java/group_13/game_store/repository/GameCopyRepository.java
+++ game_store/src/main/java/group_13/game_store/repository/GameCopyRepository.java
@@ -1,9 +1,14 @@
 package group_13.game_store.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import group_13.game_store.model.GameCopy;
 
 public interface GameCopyRepository extends CrudRepository<GameCopy, Integer> {
     // allows instantiation of an gameCopy instance that is stored in the local database by its unique ID
     public GameCopy findByCopyID(int copyID);
+
+    // returns all game copies associated with order id
+    public List<GameCopy> findByOrder_OrderID(int orderID);
 }
--- game_store/src/main/java/group_13/game_store/service/OrderManagementService.java
+++ game_store/src/main/java/group_13/game_store/service/OrderManagementService.java
@@ -6,16 +6,21 @@ import org.springframework.web.server.ResponseStatusException;
 import jakarta.transaction.Transactional;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 //import java.util.ArrayList;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import group_13.game_store.model.Customer;
 import group_13.game_store.model.Order;
 import group_13.game_store.model.Game;
+import group_13.game_store.model.GameCopy;
 //import group_13.game_store.model.GameCopy;
 import group_13.game_store.repository.CustomerRepository;
+import group_13.game_store.repository.GameCopyRepository;
 import group_13.game_store.repository.OrderRepository;
 import group_13.game_store.repository.GameRepository;
 //import group_13.game_store.repository.GameCopyRepository;
@@ -33,6 +38,9 @@ public class OrderManagementService {
     @Autowired
     private GameRepository gameRepo;
 
+    @Autowired
+    private GameCopyRepository copyRepo;
+
 
     @Transactional
     public Order getOrderById(int orderId) {
@@ -46,22 +54,28 @@ public class OrderManagementService {
     
 
     @Transactional
-    public Order returnOrder(int orderID, int gameID, Date dateToReturn)  {
+    public Order returnOrder(int orderID, Date dateToReturn)  {
         // validation
         // check if order exists
         Order orderToReturn = orderRepo.findByOrderID(orderID);
         if (orderToReturn == null) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No order with order ID " + orderID + ".");
         }
-        // check if game exists
-        Game gameToReturn = gameRepo.findByGameID(gameID);
-        if (gameToReturn == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No game with game ID " + gameID + ".");
-        }
-        // check if order was already returned
-        if (orderToReturn.isIsReturned() == true) {
+
+         // check if order was already returned
+         if (orderToReturn.isIsReturned() == true) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Order " + orderID + " was already returned.");
         }
+
+        List<GameCopy> gameCopiesOfOrder = copyRepo.findByOrder_OrderID(orderID);
+
+        // check if games from this order exist
+        /* for (GameCopy orderCopy : gameCopiesOfOrder) {
+            Game gameToReturn = gameRepo.findByGameID(orderCopy.getGame().getGameID());
+            if (gameToReturn == null) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No game with game ID " + gameToReturn.getGameID() + ".");
+            }
+        } */
 
         // check if 7 days passed after the purchas within a set amount of milliseconds
         //Date dateToReturn = Date.valueOf(LocalDate.now());
@@ -70,14 +84,20 @@ public class OrderManagementService {
         long millisecondsInDay = 1000 * 60 * 60 * 24;
         long daysPassedSincePurchase = millisecondsElapsedSincePurchase/millisecondsInDay;
         if (daysPassedSincePurchase <= 7) {
-             // increment stock count of the Game
-            int currentStockOfGame = gameToReturn.getStock();
-            gameToReturn.setStock(currentStockOfGame + 1);
+
+            for (GameCopy copy : gameCopiesOfOrder) {
+                Game gameStockToUpdate = gameRepo.findByGameID(copy.getGame().getGameID());
+                int gameCurrentStock = gameStockToUpdate.getStock();
+                // increment stock count of the Game
+                gameStockToUpdate.setStock(gameCurrentStock + 1);
+                // save these changes in the database after each iteration
+                gameStockToUpdate = gameRepo.save(gameStockToUpdate);
+            }
+
             // add returnDate of Order and modifying the isReturned status of the Order
             orderToReturn.setReturnDate(dateToReturn);
 
             // save these changes in the database
-            gameToReturn = gameRepo.save(gameToReturn);
             orderToReturn = orderRepo.save(orderToReturn);
 
             return orderToReturn;
