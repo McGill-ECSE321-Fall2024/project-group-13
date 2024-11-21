cca13cbec0fa2913e3d26a6ef8ffdb1017ef8636
WLBlackmore <william.blackmore@mail.mcgill.ca>
2024-11-21 14:58:32 -0500

    initial landing page bg image set

--- GameStoreFrontend/src/views/HomeView.vue
+++ GameStoreFrontend/src/views/HomeView.vue
@@ -1,8 +1,13 @@
 <template>
-    <div class="home">
-        <h1>Home Page</h1>
-        <p>Welcome to the Game Store!</p>
-    </div>
+    <section class="title">
+        <div class="backgroundImage">
+            <div class="titleFlex">
+                <div class="titleText">
+                    <h1>404 Games Not Found</h1>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
@@ -12,8 +17,36 @@ export default {
 </script>
 
 <style scoped>
-.home {
-    text-align: center;
-    margin-top: 50px;
-}
+
+    .backgroundImage {
+        background-image: url('../assets/TitleBackground.jpg');
+        background-size: cover;
+        background-position: center center;
+        background-repeat: no-repeat;
+        height: 825px;
+        width: 100%;
+    }  
+
+    .titleFlex {
+        display: flex;
+        height: 825px;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .titleText>*{
+        color: aliceblue;
+        font-size: 5rem;
+    }
+
+    h1{
+        font-weight: 500;
+    }
+
+    .titleText>h3{
+    font-size: 1.5rem;
+    font-weight: normal;
+    margin-left: 5px;
+    }
+
 </style>
