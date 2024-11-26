53a772e8ce55e88a1c184c96f3fa56f41a92cab1
PromitHossain <promit.hossain@mail.mcgill.ca>
2024-11-26 10:52:23 -0500

    Forgot to include payment info and address info for the default customer account.

--- game_store/src/main/java/group_13/game_store/initializer/DataInitializer.java
+++ game_store/src/main/java/group_13/game_store/initializer/DataInitializer.java
@@ -1,12 +1,18 @@
 package group_13.game_store.initializer;
 
+import java.sql.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import group_13.game_store.model.Address;
 import group_13.game_store.model.Customer;
 import group_13.game_store.model.Owner;
+import group_13.game_store.model.PaymentInformation;
+import group_13.game_store.repository.AddressRepository;
 import group_13.game_store.repository.CustomerRepository;
 import group_13.game_store.repository.OwnerRepository;
+import group_13.game_store.repository.PaymentInformationRepository;
 import group_13.game_store.service.AccountService;
 import jakarta.annotation.PostConstruct;
 
@@ -18,6 +24,12 @@ public class DataInitializer {
 
     @Autowired
     private AccountService accountService;
+
+    @Autowired
+    private AddressRepository addressRepo;
+
+    @Autowired
+    private PaymentInformationRepository paymentInfoRepo;
 
     @Autowired
     private CustomerRepository customerRepo;
@@ -34,8 +46,20 @@ public class DataInitializer {
         }
 
         if (customerRepo.findByUsername("defaultCustomer") == null) {
+
+            Address savedAddress = new Address("Sherbrooke St W", "H3A0G4", 845, "Montreal", "Quebec", "Canada", 0);
+            addressRepo.save(savedAddress);
+
+            PaymentInformation savedPaymentInformation = new PaymentInformation("1234123412341234", "Default Customer",
+                Date.valueOf("2027-10-11"), 123, savedAddress);
+            paymentInfoRepo.save(savedPaymentInformation);
+
             String hashedPassword = accountService.hashPassword("custom3rPassword");
             Customer defaultCustomer = new Customer("Default Customer", "defaultCustomer", "customer@gmail.com", hashedPassword, "123-456-7890");
+
+            defaultCustomer.setAddress(savedAddress);
+            defaultCustomer.setPaymentInformation(savedPaymentInformation);
+            
             customerRepo.save(defaultCustomer);
         }
     }
